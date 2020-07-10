8ceaf047918ad11da3b38afa01357c5bda5f232b
zhangmingyin <547805758@qq.com>
2020-07-10 19:35:33 +0800

    封装tabbar，网络请求和navbar

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,39 @@
 <template>
-  <h2>首页</h2>
+  <div id="app">
+    <nav-bar class="home-nav">
+      <div slot="center">购物车</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
+import {getHomeMultiData} from 'network/home'
   export default {
-    name: "Home"
+    name: "Home",
+    data(){
+      return {
+        banner:[],
+        recommend:[]
+      }
+    },
+    components:{
+      NavBar
+    },
+    created(){
+      getHomeMultiData().then(res=>{
+        this.banner=res.data.banner.list;
+        this.recommend=res.data.recommend.list;
+        
+      })
+    }
   }
 </script>
 
 <style scoped>
-
+  .home-nav{
+    background-color: var(--color-tint);
+    color: #fff;
+    text-align: center;
+  }
 </style>
