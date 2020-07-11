48f38e79074beeaf4628178c4ba1ce60e9c2faa8
zhangmingyin <547805758@qq.com>
2020-07-11 21:09:20 +0800

    封装轮播图组件和在组件中渲染轮播图

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,29 +1,53 @@
 <template>
-  <div id="app">
+  <div id="home">
     <nav-bar class="home-nav">
       <div slot="center">购物车</div>
     </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend :recommends="recommends"></recommend>
+    <feature-view></feature-view>
+    <ul>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+    </ul>
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
+import HomeSwiper from './childComps/HomeSwiper'
+import Recommend from './childComps/Recommend'
+import FeatureView from './childComps/FeatureView'
+
 import {getHomeMultiData} from 'network/home'
   export default {
     name: "Home",
     data(){
       return {
-        banner:[],
-        recommend:[]
+        banners:[],
+        recommends:[]
       }
     },
     components:{
-      NavBar
+      NavBar,
+      HomeSwiper,
+      Recommend,
+      FeatureView
     },
     created(){
       getHomeMultiData().then(res=>{
-        this.banner=res.data.banner.list;
-        this.recommend=res.data.recommend.list;
+        console.log(res)
+        this.banners=res.data.banner.list;
+        this.recommends=res.data.recommend.list;
         
       })
     }
@@ -31,9 +55,17 @@ import {getHomeMultiData} from 'network/home'
 </script>
 
 <style scoped>
+  #home{
+      padding-top: 44px;
+  }
   .home-nav{
     background-color: var(--color-tint);
     color: #fff;
     text-align: center;
+    position: fixed;
+    z-index: 9;
+    left: 0;
+    top: 0;
+    right: 0;
   }
 </style>
---
+++ src/views/home/childComps/Recommend.vue
@@ -0,0 +1,50 @@
+<template>
+ <div class="recommend">
+   <div class="recommends-item" v-for="item in recommends" :key="item.index">
+     <a :href="item.link">
+       <img :src="item.image" alt="">
+       <div>{{item.title}}</div>
+     </a>
+   </div>
+ </div>
+</template>
+
+<script>
+ export default {
+  name: '',
+  data () {
+   return {
+
+   }
+  },
+  props:{
+    recommends:{
+      type:Array,
+      default(){
+        return []
+      }
+    }
+  },
+  components: {
+
+  }
+ }
+</script>
+
+<style scoped>
+  .recommend{
+    display: flex;
+    text-align: center;
+    font-size: 12px;
+    padding: 10px 0px 20px;
+    border-bottom: 9px solid #eee;
+  }
+  .recommends-item{
+    flex:1
+  }
+  .recommends-item img{
+    width: 70px;
+    height: 70px;
+    margin-bottom: 10px;
+  }
+</style>
